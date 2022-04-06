af9ab08cdea5a7e8b11949e57abb576ce41cc8d9
vidapogosoft <vidapogosoft@hotmail.com>
2022-04-05 23:00:19 -0500

    Talleres dia 5
    Planificacion agil

---
+++ taller/do/NetBDD1/Steps/TestSimpleSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace NetBDD1.Steps
+{
+    [Binding]
+    public class TestSimpleSteps
+    {
+        [Given(@"Abre aplicacion de escritorio en windows")]
+        public void GivenAbreAplicacionDeEscritorioEnWindows()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Ingreso Identificacion de la persona (.*)")]
+        public void WhenIngresoIdentificacionDeLaPersona(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Ingreso Nombres de la persona Victor Portugal")]
+        public void WhenIngresoNombresDeLaPersonaVictorPortugal()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Ingreso Direccion de la persona Nor-Oeste Miraflores Ave Principal")]
+        public void WhenIngresoDireccionDeLaPersonaNor_OesteMirafloresAvePrincipal()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Selecciono Estado civil Casado")]
+        public void WhenSeleccionoEstadoCivilCasado()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Presiono Boton de Confirmar Datos")]
+        public void WhenPresionoBotonDeConfirmarDatos()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Visualizo los resultados en la seccion Datos Registrados (.*)")]
+        public void ThenVisualizoLosResultadosEnLaSeccionDatosRegistrados(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
