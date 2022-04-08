4838e9b19791b00428360ddeccfac5f55b78d915
vidapogosoft <vidapogosoft@hotmail.com>
2022-04-07 23:07:34 -0500

    Talleres dia 5

--- taller/do/NetBDD1/Steps/TestSimpleSteps.cs
+++ taller/do/NetBDD1/Steps/TestSimpleSteps.cs
@@ -1,51 +1,72 @@
 ﻿using System;
 using TechTalk.SpecFlow;
 
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+using NUnit.Framework;
+
 namespace NetBDD1.Steps
 {
     [Binding]
     public class TestSimpleSteps
     {
+        private WindowsDriver<WindowsElement> _driver;
+
         [Given(@"Abre aplicacion de escritorio en windows")]
         public void GivenAbreAplicacionDeEscritorioEnWindows()
         {
-            ScenarioContext.Current.Pending();
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", "D:\\vidapogosoft\\cursos\\2022\\sinergiass\\CSDAbril2022\\herramientas\\AppWinForm1.exe");
+            options.AddAdditionalCapability("deviceName", "HP-VPR2");
+
+            _driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
         
-        [When(@"Ingreso Identificacion de la persona (.*)")]
-        public void WhenIngresoIdentificacionDeLaPersona(int p0)
+        [When(@"Ingreso Identificacion de la persona 0919172551")]
+        public void WhenIngresoIdentificacionDeLaPersona()
         {
-            ScenarioContext.Current.Pending();
+            _driver.FindElementByAccessibilityId("TxtIdentificacion").Click();
+            _driver.FindElementByAccessibilityId("TxtIdentificacion").SendKeys("0919172551");
         }
         
         [When(@"Ingreso Nombres de la persona Victor Portugal")]
         public void WhenIngresoNombresDeLaPersonaVictorPortugal()
         {
-            ScenarioContext.Current.Pending();
+            _driver.FindElementByAccessibilityId("TxtNombres").Click();
+            _driver.FindElementByAccessibilityId("TxtNombres").SendKeys("Victor Portugal");
+
         }
-        
+
         [When(@"Ingreso Direccion de la persona Nor-Oeste Miraflores Ave Principal")]
         public void WhenIngresoDireccionDeLaPersonaNor_OesteMirafloresAvePrincipal()
         {
-            ScenarioContext.Current.Pending();
+            _driver.FindElementByAccessibilityId("TxtDireccion").Click();
+            _driver.FindElementByAccessibilityId("TxtDireccion").SendKeys("Nor-Oeste Miraflores Ave Principal");
+
         }
-        
+
         [When(@"Selecciono Estado civil Casado")]
         public void WhenSeleccionoEstadoCivilCasado()
         {
-            ScenarioContext.Current.Pending();
+            _driver.FindElementByAccessibilityId("RdCasado").Click();
         }
         
         [When(@"Presiono Boton de Confirmar Datos")]
         public void WhenPresionoBotonDeConfirmarDatos()
         {
-            ScenarioContext.Current.Pending();
+            _driver.FindElementByName("Confirmar Datos").Click();
         }
         
-        [Then(@"Visualizo los resultados en la seccion Datos Registrados (.*)")]
-        public void ThenVisualizoLosResultadosEnLaSeccionDatosRegistrados(int p0)
+        [Then(@"Visualizo los resultados en la seccion Datos Registrados 0919172551")]
+        public void ThenVisualizoLosResultadosEnLaSeccionDatosRegistrados()
         {
-            ScenarioContext.Current.Pending();
+            string TextIdentificacion = _driver.FindElementByAccessibilityId("LblIdentificacion").Text;
+
+            Assert.AreEqual("0919172551", TextIdentificacion);
+
         }
     }
 }
